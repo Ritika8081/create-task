94b75a1929d2d0694d1ca8110bcfa2d5716b57e1
Ritika Mishra <103934960+Ritika8081@users.noreply.github.com>
2023-02-17 16:46:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+import ReactDOM from "react-dom";
+import App from './App';
+
+import "./index.css";
+
+
+import ListCom from "./ListCom";
+
+
+import ToDoList from "./ToDoList";
+
+
+
+
+ReactDOM.render(<ToDoList />, document.getElementById("root"));
+
